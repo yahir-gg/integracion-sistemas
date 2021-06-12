4db6e589d503b9de6ffbce7a00b9984995f9c235
Yahir Garcia <yahirgarcia1996@gmail.com>
2021-06-11 21:59:45 -0500

    ingresa,consulta,elimina, IGrafica

--- twi.py
+++ twi.py
@@ -11,8 +11,23 @@ from spyne.server.twisted import TwistedWebResource
 from twisted.internet import reactor
 from twisted.web.server import Site
 import xml.etree.cElementTree as ET
+from lxml import etree
+from StringIO import StringIO 
+ 
+dtd = etree.DTD(StringIO("""
+<!ELEMENT abarrotes (producto*)>
+<!ELEMENT producto (nombre, marca, descripcion,precio)>
+<!ATTLIST producto id CDATA #REQUIRED>
+<!ELEMENT nombre (#PCDATA)>
+<!ELEMENT marca (#PCDATA)>
+<!ELEMENT descripcion (#PCDATA)>
+<!ELEMENT precio (#PCDATA)>
+""")) 
 
-
+def validar_xml(archivo_xml_temp):
+    mytree = etree.parse(archivo_xml_temp)
+    determinante = dtd.validate(mytree)
+    return determinante
 
 class HelloWorldService(ServiceBase):
     @srpc(Unicode, Integer, _returns=Iterable(Unicode))
@@ -22,18 +37,67 @@ class HelloWorldService(ServiceBase):
 
     @srpc(Unicode, Unicode,Unicode, Unicode, _returns= Unicode)
     def addPd(nombre_pm,marca_pm,desc_pm,precio_pm):
+        parametros=[]
+        parametros.append(nombre_pm)
+        parametros.append(marca_pm)
+        parametros.append(desc_pm)
+        parametros.append(precio_pm)
         tree = ET.parse('productos.xml')
         root = tree.getroot()
-        producto = ET.SubElement(root, "producto")
+        id_new_nodo = len(root.getchildren())+1
+        producto = ET.SubElement(root, "producto",id=str(id_new_nodo))
         ET.SubElement(producto, "nombre").text = nombre_pm
         ET.SubElement(producto, "marca").text = marca_pm
         ET.SubElement(producto, "descripcion").text = desc_pm
         ET.SubElement(producto, "precio").text = precio_pm
         ET.ElementTree(root)
-        tree.write("./productos.xml")
-        msj = "Producto ingresado"
-        return nombre_pm
+        tree.write("./productos_tmp.xml")
+        nameF = "productos_tmp.xml"
+        validacion = validar_xml(nameF)
+        evalido = bool
+        for e in parametros:
+            if e=="":
+                evalido = False
+                break
+            else:
+                evalido = True
 
+
+        if validacion == True and evalido == True:
+            tree.write("./productos.xml")
+            msj = "Archivo XML valido. El producto ha sido agregado"
+            return msj
+        else:
+            msj = "Archivo XML invalido. No se agrego el producto"
+            return msj
+
+    @srpc(Unicode, _returns= Iterable(Unicode))
+    def consultar_pd(id_consultar):
+        tree = ET.parse('productos.xml')
+        root = tree.getroot()
+        pen = root.find(".//producto[@id='"+id_consultar+"']")
+        hijos = pen.getchildren()
+        xml_lt=[]
+        xml_str=""
+        for e in hijos:
+              xml_lt.append(e.text)
+        
+        for e in xml_lt:
+            xml_str=xml_str+e+"\n"
+
+        return xml_lt[0],xml_lt[1],xml_lt[2],xml_lt[3]
+
+    @srpc(Unicode, _returns = Unicode)
+    def deletePd(id_pt):
+        tree = ET.parse('productos.xml')
+        root = tree.getroot()
+        #producto = ET.SubElement(root, "producto")
+        for producto in root.findall('producto'):
+            idP = producto.attrib['id']
+            if idP == id_pt:
+                root.remove(producto)
+        tree.write("./productos.xml")
+        return "Producto eliminado"
 
 application = Application([HelloWorldService],
                           tns='spyne.examples.hello',
--- twicliente.py
+++ twicliente.py
@@ -1,26 +1,27 @@
 from zeep import Client
 import tkinter as tk
 import xml.etree.cElementTree as ET
+
 # Empieza interfaz
 raiz = tk.Tk()
 raiz.title("Cliente")
-# raiz.geometry("650x350")
+raiz.geometry("400x300")
 miFrame=tk.Frame()
-miFrame.pack()
-miFrame.config(width="650", heigh="350")
+#miFrame.pack()
+miFrame.config(width="350", heigh="350")
 
-frame_add_pd=tk.Frame()
-frame_add_pd.pack()
-frame_add_pd.config(width="650",heigh="350")
-
-
+# Anadir producto
 def save_data(c_nombre,c_marca,c_desc,c_precio):
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
     cliente = Client('http://localhost:8000/?wsdl')
     puno = c_nombre.get()
-    pdos = c_marca.get()
+    pdos = c_marca.get()    
     ptres = c_desc.get()
     pcuatro = c_precio.get()
     res = cliente.service.addPd(puno,pdos,ptres,pcuatro)
+    l_nombre=tk.Label(vdos,text=res)
+    l_nombre.grid(row=0,column=0,padx=10,pady=10)
     print('Return:',res)
 
     #cliente.service.say_hello('audifonos',1)
@@ -32,42 +33,91 @@ def save_data(c_nombre,c_marca,c_desc,c_precio):
     arbol = ET.ElementTree(root)
     arbol.write("./prueba.xml")"""
 
-
 def ingresar_producto():
-    l_nombre=tk.Label(miFrame,text="Nombre del producto")
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
+    l_nombre=tk.Label(vdos,text="Nombre del producto")
     l_nombre.grid(row=0,column=0,padx=10,pady=10)
-    c_nombre = tk.Entry(miFrame)
+    c_nombre = tk.Entry(vdos)
     c_nombre.grid(row=0,column=1,padx=10,pady=10)
 
-    l_marca= tk.Label(miFrame,text="Marca")
+    l_marca= tk.Label(vdos,text="Marca")
     l_marca.grid(row=1,column=0,padx=10,pady=10)
-    c_marca = tk.Entry(miFrame)
+    c_marca = tk.Entry(vdos)
     c_marca.grid(row=1,column=1,padx=10,pady=10)
     
-    l_desc=tk.Label(miFrame,text="Descripcion")
+    l_desc=tk.Label(vdos,text="Descripcion")
     l_desc.grid(row=2,column=0,padx=10,pady=10)
-    c_desc = tk.Entry(miFrame)
+    c_desc = tk.Entry(vdos)
     c_desc.grid(row=2,column=1,padx=10,pady=10)
     
-    l_precio=tk.Label(miFrame,text="Precio del producto")
+    l_precio=tk.Label(vdos,text="Precio del producto")
     l_precio.grid(row=3,column=0,padx=10,pady=10)
-    c_precio = tk.Entry(miFrame)
+    c_precio = tk.Entry(vdos)
     c_precio.grid(row=3,column=1,padx=10,pady=10)
     
-    botonenvio = tk.Button(raiz,text="Enviar",command=lambda:save_data(c_nombre,c_marca,c_desc,c_precio))
-    botonenvio.pack()
+    botonenvio = tk.Button(vdos,text="Enviar",command=lambda:save_data(c_nombre,c_marca,c_desc,c_precio))
+    botonenvio.grid(row=4,column=0,padx=10,pady=10)
+# consultar producto
+def send_id(id_to_send):
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
+    cliente = Client('http://localhost:8000/?wsdl')
+    puno = id_to_send.get()
+    res = cliente.service.consultar_pd(puno)
+    l_n = tk.Label(vdos,text="Producto: "+res[0])
+    l_n.grid(row=1,column=0,padx=10,pady=2)
+    l_m = tk.Label(vdos,text="Marca: "+res[1])
+    l_m.grid(row=2,column=0,padx=10,pady=2)
+    l_d = tk.Label(vdos,text="Descripcion: "+res[2])
+    l_d.grid(row=3,column=0,padx=10,pady=2)
+    l_p = tk.Label(vdos,text="Precio: "+res[3])
+    l_p.grid(row=4,column=0,padx=10,pady=2)
+    print('Return:',res[0])
 
-botonaddPd = tk.Button(raiz,text="Aadir producto",command=ingresar_producto)
-botonaddPd.pack()
 
+def consultar_producto():
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
+    l_id = tk.Label(vdos,text="Ingrese el ID del producto a consultar")
+    l_id.grid(row=0,column=0,padx=10,pady=10)
+    c_id = tk.Entry(vdos)
+    c_id.grid(row=0,column=1,padx=10,pady=10)
+    botonenvio = tk.Button(vdos,text="Enviar consulta",command=lambda:send_id(c_id))
+    botonenvio.grid(row=1,column=0,padx=10,pady=10)
 
+# eliminar producto
+def save_delete(c_ID):
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
+    cliente = Client('http://localhost:8000/?wsdl')
+    uno = c_ID.get()
+    res = cliente.service.deletePd(uno)
+    l_ID = tk.Label(vdos, text= res)
+    l_ID.grid(row=0,column=0,padx=10,pady=10)
+    print('Return:',res)
 
-    
+def ingresaID():
+    vdos = tk.Toplevel()
+    vdos.config(width="350",heigh="350")
+    l_ID = tk.Label(vdos, text= "Ingrese ID del producto")
+    l_ID.grid(row=0,column=0,padx=10,pady=10)
+    c_ID = tk.Entry(vdos)
+    c_ID.grid(row=0,column=1,padx=10,pady=10)
+    botonenvia = tk.Button(vdos,text="Enviar elimina",command=lambda:save_delete(c_ID))
+    botonenvia.grid(row=1,column=0,padx=10,pady=10)
 
+botonaddPd = tk.Button(raiz,text="Aadir producto",height = 3, width = 20,command=ingresar_producto)
+botonaddPd.grid(row=0,column=0,padx=10,pady=10)
 
+boton_consulta = tk.Button(raiz,text="Consultar producto",height = 3, width = 20,command=consultar_producto)
+boton_consulta.grid(row=0,column=1,padx=10,pady=10)
 
+botondeletePd = tk.Button(raiz,text="Eliminar producto",height = 3, width = 20,command=ingresaID)
+botondeletePd.grid(row=1,column=0,padx=10,pady=10)
 
-
+botondeletePd = tk.Button(raiz,text="Actualizar producto",height = 3, width = 20,command=ingresaID)
+botondeletePd.grid(row=1,column=1,padx=10,pady=10)
 tk.mainloop()
 # Termina interfaz
 
